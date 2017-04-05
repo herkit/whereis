80555ffa4d0aa9317c66b36236065b85fcef8e32
Henrik Grotle <henrik.grotle@arasoft.no>
2017-04-05 23:59:43 +0200

    configurable admin password

--- migrations/20170208232540_addadmin.js
+++ migrations/20170208232540_addadmin.js
@@ -1,10 +1,11 @@
 
 exports.up = function(knex, Promise) {
+  var password = process.env.WHEREIS_ADMIN_PASSWORD || 'af039452va234';
   return Promise.all([
     knex('users').
     insert({ 
       username: 'admin', 
-      password: 'admin' 
+      password: password
     })
   ])
 };
